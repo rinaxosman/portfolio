1674ce879e81b1545fbfdcd26901898209007bc7
Rina <94803092+rinaxosman@users.noreply.github.com>
2023-06-11 12:19:11 -0400

    menu function

--- scripts.js
+++ scripts.js
@@ -10,3 +10,14 @@ contactLink.addEventListener('click', (event) => {
   const footerElement = document.querySelector('#footer');
   footerElement.scrollIntoView({ behavior: 'smooth' });
 });
+
+
+function updatemenu() {
+  if (document.getElementById('responsive-menu').checked == true) {
+    document.getElementById('menu').style.borderBottomRightRadius = '0';
+    document.getElementById('menu').style.borderBottomLeftRadius = '0';
+  }else{
+    document.getElementById('menu').style.borderRadius = '5px';
+  }
+}
+
